2394194682aeca2493f72883fe02f73766d3c137
Caglar Yalcin <github@caglaryalcin.com>
2024-10-08 22:13:46 +0300

    Update generate.psm1

--- generate.psm1
+++ generate.psm1
@@ -2,4 +2,5 @@ $bytes = New-Object Byte[] 128
 [System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($bytes)
 $base64 = [Convert]::ToBase64String($bytes)
 
-Write-Host "`nBase64 code: $base64"
+Write-Host "`nBase64 code:" -ForegroundColor Green -BackgroundColor Black -NoNewline
+Write-Host " $base64"
